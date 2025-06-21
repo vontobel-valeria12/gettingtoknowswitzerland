bb702f4a7981a8e74f042d14bc0708f48f874a08
vontobel-valeria12 <valeriavontobel@gmail.com>
2025-06-21 18:47:30 +0200

    Criar javascript.js

---
+++ javascript.js
@@ -0,0 +1,5 @@
+<img src="imagem/nova-foto.jpg" alt="Descrição" class="scroll-in" />
+<iframe class="scroll-in" width="560" height="315"
+  src="https://www.youtube.com/embed/VIDEO_ID" 
+  title="YouTube video player" frameborder="0" allowfullscreen>
+</iframe>
